19f90a16b5791da5cd049051509d1b1054d82ac0
malysz <malysz1997@gmail.com>
2017-10-27 17:16:05 +0200

    Update Car.java
    duplicate code was extracted

--- src/main/agh/cs/lab2/Car.java
+++ src/main/agh/cs/lab2/Car.java
@@ -7,6 +7,14 @@ public class Car {
     private Position position;
     private MapDirection direction;
 
+    public Car(IWorldMap map){
+
+    }
+
+    public Car(IWorldMap map, int x, int y){
+
+    }
+
     public Car(int x, int y){
         Position pos = new Position(2,2);
         this.position=pos;
@@ -21,30 +29,25 @@ public class Car {
     public void move(MoveDirection direction){
         Position low = new Position(0,0);
         Position high = new Position(4,4);
+        Position pos2 = new Position(0,0);
         switch(direction){
             case Backward:
                 switch (this.direction) {
                     case North:
-                        Position pos2 = new Position(0, -1);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
+                        pos2 = new Position(0, -1);
                         break;
                     case South:
                         pos2 = new Position(0, 1);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
                         break;
                     case West:
                         pos2 = new Position(1, 0);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
                         break;
                     case East:
                         pos2 = new Position(-1, 0);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
                         break;
                 }
+                if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
+                    this.position=this.position.add(pos2);
                 break;
             case Right:
                 this.direction=this.direction.next();
@@ -55,26 +58,21 @@ public class Car {
             case Forward:
                 switch (this.direction) {
                     case North:
-                        Position pos2 = new Position(0, 1);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
+                        pos2 = new Position(0, 1);
                         break;
                     case South:
                         pos2 = new Position(0, -1);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
                         break;
                     case West:
-                        pos2 = new Position(-1, 0);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
+                        pos2 = new Position(-1, 0);;
                         break;
                     case East:
                         pos2 = new Position(1, 0);
-                        if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
-                            this.position=this.position.add(pos2);
                         break;
                 }
+                if (this.position.add(pos2).larger(low) && this.position.add(pos2).smaller(high))
+                    this.position=this.position.add(pos2);
+                break;
         }
     }
 }
